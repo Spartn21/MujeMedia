7d11ae4421514646013ebe1bc6c64fc8925153ba
Spartn21 <jeremymulungi@mgila.com>
2024-11-13 12:19:21 +0300

    nextconfig4

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-// next.config.js
-module.exports = {
-    reactStrictMode: true,
-  };
-  
+/** @type {import('next.).NextConfig} */
+const nextConfig = {
+    eslint:{
+        ignoreDuringBuilds: true
+    },
+}
+
+module.exports = nextConfig
